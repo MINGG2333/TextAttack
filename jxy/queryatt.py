1ecaf889ea00f418c9773b4df49e5f9faacd61f4
MINGG2333 <xyz_7106@foxmail.com>
2021-12-09 09:33:35 +0000

    in colab

--- jxy/queryatt.py
+++ jxy/queryatt.py
@@ -61,12 +61,12 @@ attack_results = attacker.attack_dataset()
 #       print(f'{num_successes} of 10 successes complete.')
 
 import pandas as pd
-pd.options.display.max_colwidth = 480 # increase colum width so we can actually read the examples
+# pd.options.display.max_colwidth = 480 # increase colum width so we can actually read the examples
 
-logger = CSVLogger(color_method='html')
+# logger = CSVLogger(color_method='html')
 
-for result in attack_results:
-    logger.log_attack_result(result)
+# for result in attack_results:
+#     logger.log_attack_result(result)
 
-from IPython.core.display import display, HTML
-display(HTML(logger.df[['original_text', 'perturbed_text']].to_html(escape=False)))
+# from IPython.core.display import display, HTML
+# display(HTML(logger.df[['original_text', 'perturbed_text']].to_html(escape=False)))
